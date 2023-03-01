65fbc1768bc8140b9440f87ecc78e5e4814e3ea3
umitkalemkus <umitkalemkus@yahoo.com>
2023-03-01 08:31:46 +0000

    done

---
+++ src/test/java/getRequest/Get13.java
@@ -0,0 +1,19 @@
+package getRequest;
+
+import org.junit.Test;
+
+public class Get13 {
+
+    @Test
+    public void get12(){
+
+
+        //Step 1: Set URL
+        //https://gorest.co.in/public/v2/users/702440
+        specification.pathParams("usersPath","users",
+                "idPath","702440");
+
+
+
+
+    }
